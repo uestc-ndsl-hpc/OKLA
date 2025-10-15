2839cbeb7bd4d056454ab511c1479fe60d265559
ruinique <rui1@std.uestc.edu.cn>
2025-10-15 20:46:21 +0800

    modify main.cu to test more simply

--- src/main.cu
+++ src/main.cu
@@ -8,21 +8,20 @@
 #include "matrix_ops/matrix_ops.cuh"
 #include "osla_warppers/osla_warppers.cuh"
 
-using demo_type = float;
-
+template <typename T>
 void warm_up() {
     if (util::Logger::is_verbose()) {
         util::Logger::println("[info] Performing GEMM warm-up");
     }
     const int n_warmup = 16384;
-    thrust::device_vector<demo_type> d_A(n_warmup * n_warmup, 1.0);
-    thrust::device_vector<demo_type> d_B(n_warmup * n_warmup, 1.0);
-    thrust::device_vector<demo_type> d_C(n_warmup * n_warmup, 0.0);
-    demo_type alpha = 1.0;
-    demo_type beta = 0.0;
+    thrust::device_vector<T> d_A(n_warmup * n_warmup, 1.0);
+    thrust::device_vector<T> d_B(n_warmup * n_warmup, 1.0);
+    thrust::device_vector<T> d_C(n_warmup * n_warmup, 0.0);
+    T alpha = 1.0;
+    T beta = 0.0;
     common::CublasHandle handle;
     for (int i = 0; i < 10; i++) {
-        if constexpr (std::is_same_v<demo_type, float>) {
+        if constexpr (std::is_same_v<T, float>) {
             cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, n_warmup, n_warmup,
                         n_warmup, (const float*)&alpha,
                         (const float*)thrust::raw_pointer_cast(d_A.data()),
@@ -44,31 +43,27 @@ void warm_up() {
     util::Logger::println("[info] Warm-up finished");
 }
 
-int main(int argc, char** argv) {
-    // cli args parse
-    argh::parser cmdl(argv);
-    auto n = (size_t)8192;
-    cmdl({"-n", "--size"}, 4) >> n;
-    auto verbose = cmdl[{"-v", "--verbose"}];
-    util::Logger::init(verbose);
-    util::Logger::print_environment_info();
-
+template <typename T>
+int benchmark(argh::parser& cmdl, size_t n) {
     if (!cmdl[{"--nowarmup"}]) {
-        warm_up();
+        warm_up<T>();
     }
 
     // create positive-definite matrix A
-    auto A = matrix_ops::create_symmetric_random<demo_type>(n);
+    auto A = matrix_ops::create_symmetric_random<T>(n);
     // A = A + n * I
     thrust::for_each(thrust::counting_iterator<size_t>(0),
                      thrust::counting_iterator<size_t>(n),
                      [A_ptr = A.data(), n] __device__(size_t i) {
-                         A_ptr[i * n + i] += static_cast<demo_type>(n);
+                         A_ptr[i * n + i] += static_cast<T>(n);
                      });
+
+    thrust::host_vector<T> h_A = A;
 
     auto cusolver_handle = common::CusolverDnHandle();
 
     if (cmdl[{"--test-cusolver"}]) {
+        A = h_A;  // reset A
         util::Logger::tic("Cusolver Cholesky Factorization");
         auto status =
             matrix_ops::cusolver::cholesky(cusolver_handle, A.data(), n);
@@ -83,6 +78,7 @@ int main(int argc, char** argv) {
     }
 
     if (cmdl[{"--test-osla"}]) {
+        A = h_A;  // reset A
         util::Logger::tic("OSLA Cholesky Factorization");
         auto status = matrix_ops::osla::cholesky(cusolver_handle, A.data(), n,
                                                  n, 8192, 1024);
@@ -94,6 +90,27 @@ int main(int argc, char** argv) {
                       << std::endl;
             return -1;
         }
+    }
+    return 0;
+}
+
+int main(int argc, char** argv) {
+    // cli args parse
+    argh::parser cmdl(argv);
+    auto n = (size_t)8192;
+    cmdl({"-n", "--size"}, 4) >> n;
+    auto verbose = cmdl[{"-v", "--verbose"}];
+    util::Logger::init(verbose);
+    util::Logger::print_environment_info();
+
+    if (cmdl[{"--float"}]) {
+        return benchmark<float>(cmdl, n);
+    } else if (cmdl[{"--double"}]) {
+        return benchmark<double>(cmdl, n);
+    } else {
+        std::cerr << "Please specify --float or --double for the data type."
+                  << std::endl;
+        return -1;
     }
 
     return 0;
