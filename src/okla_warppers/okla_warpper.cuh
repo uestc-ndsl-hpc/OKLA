1959dcc6fcc05ecd0d4e740783b13dad80331e9c
ruinique <rui1@std.uestc.edu.cn>
2025-11-07 21:30:14 +0800

    Refactor potrf function to enable logging and optimize stream handling; adjust block size parameter for better performance.

---
+++ src/okla_warppers/okla_warpper.cuh
@@ -0,0 +1,119 @@
+#include <thrust/device_ptr.h>
+#include <thrust/device_vector.h>
+
+#include <cstddef>
+#include <cusolverdx.hpp>
+
+namespace matrix_ops {
+namespace okla {
+// enum for status code of okla
+enum class OklaStatus {
+    OKLA_STATUS_SUCCESS = 0,
+    OKLA_STATUS_INVALID_VALUE = -1,
+    OKLA_STATUS_EXECUTION_FAILED = -2,
+    OKLA_STATUS_NOT_INITIALIZED = -3
+};
+
+namespace common {
+
+constexpr int Arch = 890;
+
+// Convenience wrapper for cusolverdx::copy_2d functions to use in the examples
+template <class Operation, unsigned BPB = 1>
+struct io {
+    using data_type = typename Operation::a_data_type;
+
+    static constexpr unsigned int m = Operation::m_size;
+    static constexpr unsigned int n = Operation::n_size;
+    static constexpr unsigned int nrhs = Operation::k_size;
+    static constexpr unsigned int nthreads = Operation::max_threads_per_block;
+
+    // load BPB batches of A from global memory to shared memory
+    static inline __device__ void load_a(const data_type* A, const int lda,
+                                         data_type* As, const int ldas) {
+        cusolverdx::copy_2d<Operation, m, n,
+                            cusolverdx::arrangement_of_v_a<Operation>, BPB>(
+            A, lda, As, ldas);
+        __syncthreads();
+    }
+
+    // store BPB batches of A from shared memory to global memory
+    static inline __device__ void store_a(const data_type* As, const int ldas,
+                                          data_type* A, const int lda) {
+        __syncthreads();
+        cusolverdx::copy_2d<Operation, m, n,
+                            cusolverdx::arrangement_of_v_a<Operation>, BPB>(
+            As, ldas, A, lda);
+    }
+
+    // load BPB batches of B from global memory to shared memory
+    // Note that the wrapper function cannot be used for unmlq/unmqr function
+    // with right size multiplication
+    static inline __device__ void load_b(const data_type* B, const int ldb,
+                                         data_type* Bs, const int ldbs) {
+        cusolverdx::copy_2d<Operation, (m > n ? m : n), nrhs,
+                            cusolverdx::arrangement_of_v_b<Operation>, BPB>(
+            B, ldb, Bs, ldbs);
+        __syncthreads();
+    }
+
+    // store BPB batches of B from shared memory to global memory
+    // Note that the wrapper function cannot be used for unmlq/unmqr function
+    // with right size multiplication
+    static inline __device__ void store_b(const data_type* Bs, const int ldbs,
+                                          data_type* B, const int ldb) {
+        __syncthreads();
+        cusolverdx::copy_2d<Operation, (m > n ? m : n), nrhs,
+                            cusolverdx::arrangement_of_v_b<Operation>, BPB>(
+            Bs, ldbs, B, ldb);
+    }
+};
+
+}  // namespace common
+
+template <class Solver, typename DataType = typename Solver::a_data_type>
+__global__ __launch_bounds__(Solver::max_threads_per_block) void potrf_kernel(
+    DataType* A, const unsigned int lda_smem,
+    typename Solver::status_type* info) {
+    extern __shared__ unsigned char shared_mem[];
+    DataType* As = reinterpret_cast<DataType*>(shared_mem);
+
+    constexpr auto lda_gmem = Solver::m_size;
+
+    // Load data from global memory to shared memory
+    common::io<Solver>::load_a(A, lda_gmem, As, lda_smem);
+
+    Solver().execute(As, lda_smem, info);
+
+    // Store results back to global memory
+    common::io<Solver>::store_a(As, lda_smem, A, lda_gmem);
+}
+
+template <typename T>
+OklaStatus potrf(thrust::device_ptr<T> A, size_t n, size_t lda = 0) {
+    if (lda == 0) {
+        lda = n;
+    }
+
+    using namespace cusolverdx;
+    // Build a Solver description that matches the template data type T so that
+    // the cusolverdx block_execution uses the same data type (float/double)
+    // as the matrix provided by the caller. Previously Precision<double>()
+    // was hard-coded which caused DataType mismatches when T=float.
+    using Solver =
+        decltype(Size<32, 32>() + Precision<T>() + Type<type::real>() +
+                 Function<function::potrf>() + FillMode<lower>() + Block() +
+                 SM<common::Arch>() + BlockDim<256>());
+    auto lda_smem = (unsigned int)34;
+    const unsigned int sm_size = Solver::get_shared_memory_size(lda_smem);
+    thrust::device_vector<typename Solver::status_type> d_info(1);
+
+    cudaStream_t stream = nullptr;
+    cudaStreamCreateWithFlags(&stream, cudaStreamNonBlocking);
+    potrf_kernel<Solver><<<1, Solver::block_dim, sm_size, stream>>>(
+        thrust::raw_pointer_cast(A), lda_smem,
+        thrust::raw_pointer_cast(d_info.data()));
+    return OklaStatus::OKLA_STATUS_SUCCESS;
+}
+}  // namespace okla
+}  // namespace matrix_ops
--- src/osla_warppers/potrf_warppers.cuh
+++ src/osla_warppers/potrf_warppers.cuh
@@ -13,7 +13,7 @@ namespace matrix_ops {
 namespace osla {
 template <typename T>
 int potrf(const common::CusolverDnHandle& handle, thrust::device_ptr<T> A,
-          size_t n, size_t lda = 0, size_t nb = 8192, size_t b = 1024) {
+          size_t n, size_t lda = 0, size_t nb = 8192, size_t b = 64) {
     if (n <= nb) {
         return matrix_ops::cusolver::potrf<T>(handle, A, n, lda);
     }
@@ -22,7 +22,21 @@ int potrf(const common::CusolverDnHandle& handle, thrust::device_ptr<T> A,
 
     auto cublas_handle = common::CublasHandle();
 
-    util::Logger::init(true);
+    constexpr bool kEnableLogging = true;
+    util::Logger::init_timer(kEnableLogging);
+
+    // init stream for inner update operations
+    cudaStream_t stream_cusolver;
+    cudaStreamCreateWithFlags(&stream_cusolver, cudaStreamNonBlocking);
+    cudaStream_t ori_handle_cusolver_stream;
+    cusolverDnGetStream(handle, &ori_handle_cusolver_stream);
+    cusolverDnSetStream(handle, stream_cusolver);
+    
+    cudaStream_t stream_cublas;
+    cudaStreamCreateWithFlags(&stream_cublas, cudaStreamNonBlocking);
+    cudaStream_t ori_handle_cublas_stream;
+    cublasGetStream(cublas_handle, &ori_handle_cublas_stream);
+    cublasSetStream(cublas_handle, stream_cublas);
 
     for (auto outer_index = 0; outer_index < n; outer_index += nb) {
         for (auto inner_index = outer_index;
@@ -69,58 +83,88 @@ int potrf(const common::CusolverDnHandle& handle, thrust::device_ptr<T> A,
 
             // update the following panel
             constexpr bool kUseGemmForPanelUpdate = true;
-            msg = fmt::format("syrk n:{} k:{} kUseGemmForPanelUpdate: {}", b,
-                              inner_index + b - outer_index,
-                              kUseGemmForPanelUpdate);
-            util::Logger::tic(msg);
+            // When logging is disabled, fuse the two GEMMs into one to reduce launches.
             if constexpr (kUseGemmForPanelUpdate) {
-                constexpr T alpha = static_cast<T>(-1.0);
-                constexpr T beta = static_cast<T>(1.0);
-                matrix_ops::gemm(
-                    cublas_handle, b, b, inner_index + b - outer_index, alpha,
-                    A + (inner_index + b) + outer_index * lda, lda, false,
-                    A + (inner_index + b) + outer_index * lda, lda, true, beta,
-                    A + inner_index + b + (inner_index + b) * lda, lda);
-            } else {
-                if constexpr (std::is_same_v<T, float>) {
-                    float alpha = -1.0f;
-                    float beta = 1.0f;
-                    cublasSsyrk(
-                        cublas_handle, CUBLAS_FILL_MODE_LOWER, CUBLAS_OP_N, b,
-                        inner_index + b - outer_index, &alpha,
-                        thrust::raw_pointer_cast(A + (inner_index + b) +
-                                                 outer_index * lda),
-                        lda, &beta,
-                        thrust::raw_pointer_cast(A + inner_index + b +
-                                                 (inner_index + b) * lda),
-                        lda);
-                } else if constexpr (std::is_same_v<T, double>) {
-                    double alpha = -1.0;
-                    double beta = 1.0;
-                    cublasDsyrk(
-                        cublas_handle, CUBLAS_FILL_MODE_LOWER, CUBLAS_OP_N, b,
-                        inner_index + b - outer_index, &alpha,
-                        thrust::raw_pointer_cast(A + (inner_index + b) +
-                                                 outer_index * lda),
-                        lda, &beta,
-                        thrust::raw_pointer_cast(A + inner_index + b +
-                                                 (inner_index + b) * lda),
-                        lda);
+                const auto k = static_cast<size_t>(inner_index + b - outer_index);
+                const auto m_total = static_cast<size_t>(n - inner_index - b);
+                if (m_total > 0 && k > 0) {
+                    constexpr T alpha = static_cast<T>(-1.0);
+                    constexpr T beta = static_cast<T>(1.0);
+                    // Fused update: Cblk(inner_index+b : n-1, inner_index+b : inner_index+2b-1)
+                    // = beta*Cblk + alpha * W * X^T
+                    // where W = A(inner_index+b : n-1, outer_index : inner_index+b-1)
+                    //       X = A(inner_index+b : inner_index+2b-1, outer_index : inner_index+b-1)
+                    util::Logger::tic("gemm panel update");
+                    matrix_ops::gemm(
+                        cublas_handle, m_total, b, k, alpha,
+                        A + (inner_index + b) + outer_index * lda, lda, false,
+                        A + (inner_index + b) + outer_index * lda, lda, true,
+                        beta,
+                        A + (inner_index + b) + (inner_index + b) * lda, lda);
+                    util::Logger::toc("gemm panel update", m_total * k * b);
                 }
+            } else {
+                auto msg_local = fmt::format(
+                    "syrk n:{} k:{} kUseGemmForPanelUpdate: {}", b,
+                    inner_index + b - outer_index, kUseGemmForPanelUpdate);
+                util::Logger::tic(msg_local);
+                if constexpr (kUseGemmForPanelUpdate) {
+                    constexpr T alpha = static_cast<T>(-1.0);
+                    constexpr T beta = static_cast<T>(1.0);
+                    matrix_ops::gemm(
+                        cublas_handle, b, b, inner_index + b - outer_index,
+                        alpha, A + (inner_index + b) + outer_index * lda, lda,
+                        false, A + (inner_index + b) + outer_index * lda, lda,
+                        true, beta,
+                        A + inner_index + b + (inner_index + b) * lda, lda);
+                } else {
+                    if constexpr (std::is_same_v<T, float>) {
+                        float alpha = -1.0f;
+                        float beta = 1.0f;
+                        cublasSsyrk(
+                            cublas_handle, CUBLAS_FILL_MODE_LOWER,
+                            CUBLAS_OP_N, b, inner_index + b - outer_index,
+                            &alpha,
+                            thrust::raw_pointer_cast(A + (inner_index + b) +
+                                                     outer_index * lda),
+                            lda, &beta,
+                            thrust::raw_pointer_cast(
+                                A + inner_index + b +
+                                (inner_index + b) * lda),
+                            lda);
+                    } else if constexpr (std::is_same_v<T, double>) {
+                        double alpha = -1.0;
+                        double beta = 1.0;
+                        cublasDsyrk(
+                            cublas_handle, CUBLAS_FILL_MODE_LOWER,
+                            CUBLAS_OP_N, b, inner_index + b - outer_index,
+                            &alpha,
+                            thrust::raw_pointer_cast(A + (inner_index + b) +
+                                                     outer_index * lda),
+                            lda, &beta,
+                            thrust::raw_pointer_cast(
+                                A + inner_index + b +
+                                (inner_index + b) * lda),
+                            lda);
+                    }
+                }
+                util::Logger::toc(msg_local,
+                                  b * (inner_index + b - outer_index) * b);
+
+                if (inner_index + 2 * b >= n) continue;
+
+                util::Logger::tic("gemm");
+                matrix_ops::gemm(
+                    cublas_handle, n - inner_index - 2 * b, b,
+                    inner_index - outer_index + b, (T)-1.0,
+                    A + (inner_index + 2 * b) + outer_index * lda, lda, false,
+                    A + (inner_index + b) + outer_index * lda, lda, true,
+                    (T)1.0,
+                    A + (inner_index + 2 * b) + (inner_index + b) * lda, lda);
+                util::Logger::toc(
+                    "gemm", 2 * (n - inner_index - 2 * b) * b *
+                                 (inner_index - outer_index + b));
             }
-            util::Logger::toc(msg, b * (inner_index + b - outer_index) * b);
-
-            if (inner_index + 2 * b >= n) continue;
-
-            util::Logger::tic("gemm");
-            matrix_ops::gemm(
-                cublas_handle, n - inner_index - 2 * b, b,
-                inner_index - outer_index + b, (T)-1.0,
-                A + (inner_index + 2 * b) + outer_index * lda, lda, false,
-                A + (inner_index + b) + outer_index * lda, lda, true, (T)1.0,
-                A + (inner_index + 2 * b) + (inner_index + b) * lda, lda);
-            util::Logger::toc("gemm", 2 * (n - inner_index - 2 * b) * b *
-                                          (inner_index - outer_index + b));
         }
 
         if (outer_index + nb >= n) break;
